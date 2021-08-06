3b000e73d1238040b378d2ce9d91a9913681bc3b
Якубенко Михаил <yakubenko.mish@yandex.by>
2021-08-07 01:27:08 +0300

    Docker Update

--- backend/grocery_assistant/grocery_assistant/settings.py
+++ backend/grocery_assistant/grocery_assistant/settings.py
@@ -59,12 +59,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'grocery_assistant.wsgi.application'
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
